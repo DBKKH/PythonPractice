639b8fd089db9351287a9f34f0926e162bb2f2ea
hideki.koike <hideki.koike@tx-inc.com>
2022-05-08 19:19:25 +0900

    update interface sample

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -1,6 +1,45 @@
-import abc
+from abc import ABCMeta, abstractmethod
 
-class IMailSender(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def send(self, mail: Mail) -> None:
-        raise NotImplementedError()
+
+class IAnimal(metaclass=ABCMeta):
+    @abstractmethod
+    def sound(self):
+        raise NotImplementedError(self)
+
+    @abstractmethod
+    def walk(self):
+        pass
+
+
+class Cat(IAnimal):
+    def sound(self):
+        print('Nyaaa')
+
+    def walk(self):
+        super(Cat, self).walk()
+        print('suta suta')
+
+
+class Dog():
+    def sound(self):
+        print('Wow')
+
+    def walk(self):
+        print('teku teku')
+
+
+try:
+    IAnimal.register(Dog)
+except AttributeError:
+    print('No implementation')
+
+if __name__ == "__main__":
+    assert issubclass(Cat().__class__, IAnimal)
+    assert isinstance(Cat(), IAnimal)
+    Cat().sound()
+    Cat().walk()
+
+    assert issubclass(Dog().__class__, IAnimal)
+    assert isinstance(Dog(), IAnimal)
+    Dog().sound()
+    Dog().walk()
